882c390426884e11887f7502a728aeb72334e4a5
Tiago Medeiros <tiago.medeiros11@gmail.com>
2023-02-17 17:56:57 -0300

    Começo do roteamento

--- routes/accounts.js
+++ routes/accounts.js
@@ -1,8 +1,16 @@
 import express from "express";
+import { promises as fs } from "fs";
+
+const { readFile, writeFile } = fs;
 const router = express.Router();
 
 
+router.post('/', (req, res) => {
+    let account = req.body;
 
+
+    res.end();
+});
 
 
 export default router;
