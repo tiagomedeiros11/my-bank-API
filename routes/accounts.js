19cef0a010e00b31fd37ca72a12a0e58cd90e05d
Tiago Medeiros <tiago.medeiros11@gmail.com>
2023-02-22 17:54:28 -0300

    Adicionado a validação das informações quando adiciona uma conta, como tambem quando altera os dados de uma conta existente ou somente do balance

--- routes/accounts.js
+++ routes/accounts.js
@@ -10,14 +10,24 @@ const router = express.Router();
 router.post('/', async (req, res, next) => {
     try {
         let account = req.body;
+        
+        //validação dos dados obrigatorios
+        if (!account.name || account.balance == null) {
+            throw new Error("Name e Balance são obrigatorios!!!");
+        };
+        
         //variavel que realiza a leitura do arquivo accounts.json no formato JSON
         const data = JSON.parse(await readFile("accounts.json"));
+        
         // atualiza o valor da variavel (data), passando o id como primeiro item do objeto e em seguida incrementa o id+1, e traz os valores restantes do objeto.
-        account = { id: data.nextId++, ...account };
+        account = { id: data.nextId++, name: account.name, balance: account.balance  };
+        
         //atualiza o array de cadastros (accounts), inserindo um novo cadastro
         data.accounts.push(account);
+        
         //escreve no arquivo(accounts.json) a atulização da variavel (data)
         await writeFile("accounts.json", JSON.stringify(data,null, 2));
+        
         //retorna para o usuario os valores da nova conta cadastrada
         res.send(account);
         logger.info(`POST /account - ${JSON.stringify(account)}`);
@@ -57,6 +67,21 @@ router.get('/:id', async (req, res, next) => {
     }
 });
 
+//get request que busca a conta por nome
+router.get('/:nome', async (req, res, next) => {
+    try {
+        //variavel que realiza a leitura do arquivo accounts.json no formato JSON
+        const data = JSON.parse(await readFile("accounts.json"));
+        //variavel que tras do data as contas por nome informado no req.params
+        const contaIdividual = data.accounts.find(contaIdividual => contaIdividual.name === (req.params.name));
+        //deleta a informação do id do cliente trazendo somente o nome e o valor da conta
+        delete contaIdividual.id;
+        //retorna para o usuario o nome e o valor da conta solicitada
+        res.send(contaIdividual);
+    } catch (error) {
+        next(error);
+    }
+});
 
 router.delete('/:id', async (req, res, next) => {
     try {
@@ -82,14 +107,25 @@ router.put('/', async (req, res, next) => {
     try {
         //recebe os dados do body da requisição
         let account = req.body;
+        //validação dos dados obrigatorios
+        if (!account.name || account.balance == null) {
+            throw new Error("Name e Balance são obrigatorios!!!")
+        }
         //recebe a leitura do arquivo accounts.json no formato JSON
         const data = JSON.parse(await readFile("accounts.json"));
         //recebe o indice do array de accounts que tem o id a ser atualizado
         const index = data.accounts.findIndex(conta => conta.id === account.id);
-        //indice do array do acconuts localizado agora é o valor account
-        data.accounts[index] = account;
+        //validação caso o id seja inexistente
+        if (index === -1) {
+        throw new Error("Registro nao encontrado")
+        }
+
+        data.accounts[index].balance = account.balance;
+        data.accounts[index].name = account.name;
+
         //ecreve no arquivo(accounts.json) o novo valor da acconut
         await writeFile("accounts.json", JSON.stringify(data,null, 2));
+
         res.send(`O usuário ${account.name} foi atualizado com sucesso`);
         logger.info(`PUT /account - ${JSON.stringify(account)}`);
 
@@ -102,14 +138,23 @@ router.put('/', async (req, res, next) => {
 router.patch('/updateBalance', async (req, res, next) => {
     try {
         let account = req.body;
+        //validação dos dados obrigatorios
+        if (!account.id || account.balance == null) {
+            throw new Error("Id e balance precisam ser preenchido.");
+        }
         //recebe a leitura do arquivo accounts.json no formato JSON
         const data = JSON.parse(await readFile("accounts.json"));
+       
         //recebe o indice que tem o id a ser atualizado
         const index = data.accounts.findIndex(conta => conta.id === account.id);
+       
         //data recebe o novo valor do balance informado
         data.accounts[index].balance = account.balance;
+        
+       
         //ecreve no arquivo(accounts.json) o novo valor do balance
         await writeFile("accounts.json", JSON.stringify(data,null, 2));
+       
         //informa ao usuario que a o valor da conta foi atualizada
         res.send(`Saldo atualizado com sucesso!!!!`);
         logger.info(`PATCH /account / updateBalance - ${JSON.stringify(account)}`);
